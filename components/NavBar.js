ebbe1bd3432e089b629d16e4bbbad9e4749ae855
MDmubarak786 <mohammedmubarakmk@gmail.com>
2022-02-19 09:48:08 +0530

    feat(2nd-navbar):scrollbar lib , TMDB api , navbar added

---
+++ components/NavBar.js
@@ -0,0 +1,25 @@
+import requests from '../utills/request'
+import { useRouter } from 'next/router'
+
+function NavBar() {
+  const router = useRouter()
+  return (
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20 text-xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            role="button"
+            onClick={() => router.push(`/?genre=${key}`)}
+            className="last:pr-24 cursor-pointer transition duration-100 transform hover:text-white hover:scale-125 active:text-red-500 "
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
+    </nav>
+  )
+}
+
+export default NavBar
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <NavBar />
     </div>
   )
 }
